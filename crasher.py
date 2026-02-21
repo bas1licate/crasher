00030204dea6e572d4feb4ef727de592ab39de14
bas1licate <139423631+bas1licate@users.noreply.github.com>
2026-02-21 09:01:40 -0800

    works perfectly fine without a

--- crasher.py
+++ crasher.py
@@ -1,6 +1,3 @@
 import subprocess as s,os,sys,ctypes as c
-def a():
-    try:return c.windll.shell32.IsUserAnAdmin()
-    except:return 0
-if not a():c.windll.shell32.ShellExecuteW(None,"runas",sys.executable,f'"{os.path.abspath(__file__)}"',None,1)
+if not c.windll.shell32.IsUserAnAdmin():c.windll.shell32.ShellExecuteW(None,"runas",sys.executable,f'"{os.path.abspath(__file__)}"',None,1)
 s.run([f'{os.environ["userprofile"]}/nmf/notmyfault64.exe',"/bugcheck","0xE2"])
